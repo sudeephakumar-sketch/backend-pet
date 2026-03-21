91f3f8aa09f9c667d055ab5ee4872d5ecc98f16b
Sudeepha <your-email@example.com>
2026-03-21 13:52:22 +0530

    Updated CORS multiple origins

--- src/main/java/com/example/demo/CorsConfig.java
+++ src/main/java/com/example/demo/CorsConfig.java
@@ -13,10 +13,14 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://frontend-pet-lilac.vercel.app",
+                            "https://pet-frontend-app56.azurewebsites.net"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
-        };
+        }; 
     }
 }
